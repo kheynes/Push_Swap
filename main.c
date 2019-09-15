246e16470f46b41c1d1d92970e4644fcf7678347
Keziah <heyneskeziah@gmail.com>
2019-09-15 15:19:11 +0200

    Testing a main for gnl

--- main.c
+++ main.c
@@ -11,8 +11,33 @@
 /* ************************************************************************** */
 
 #include "libft/libft.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
+int		main(int ac, char **av)
+//int		main(void)
 {
-    
+	char	**line;
+	int		gnl_ret = 1;
+	int		fd = 0;
+	size_t	count = 0;
+
+	if (ac != 2)
+		printf("Please specify a file!");
+	else
+		fd = open(av[1], O_RDONLY);
+//		fd = open("short_test.txt", O_RDONLY);
+		line = malloc(sizeof(char**));
+		printf("-------Start of File-------\n");
+		while ((gnl_ret = get_next_line(fd, line)) > 0)
+		{
+			printf("%s\n", *line);
+			free(*line);
+			count++;
+		}
+		printf("-----------EOF-------------\n");
+		close(fd);
+		printf("%d lines printed\n", count);
+		printf("FIN :D\n");
+	return (0);
 }
