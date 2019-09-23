bd38376eb3973279efc25e9aa665712914b241de
Keziah Heynes <heyneskeziah@gmail.com>
2019-09-23 10:13:48 +0200

    using my get_next_line

--- checker.c
+++ checker.c
@@ -6,11 +6,31 @@
 /*   By: kheynes <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 10:46:01 by kheynes           #+#    #+#             */
-/*   Updated: 2019/09/19 10:46:14 by kheynes          ###   ########.fr       */
+/*   Updated: 2019/09/23 10:10:22 by kheynes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft/libft.h"
+#include <stdio.h>
+
 int main()
-{
-    
+{   
+    char    *line;
+    int     ret;
+    int     fd;
+	int		count;
+
+	count = 0;
+	ret = 1;
+	fd = open("test.txt", O_RDONLY);
+	while((ret = get_next_line(fd, &line) > 0))
+	{
+		free(line);
+		count++;
+	}
+	
+	close(fd);
+	printf("%d lines were read\n", count);
+	printf("OK\n");
+	return (0);	
 }
