9d6759ef284640e37ef219861594169ef29ce2a2
Keziah Heynes <heyneskeziah@gmail.com>
2019-09-24 13:49:45 +0200

    deleted a list and added a file for operation functions

--- push_swap.h
+++ push_swap.h
@@ -6,22 +6,16 @@
 /*   By: kheynes <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 10:21:20 by kheynes           #+#    #+#             */
-/*   Updated: 2019/09/23 11:05:46 by kheynes          ###   ########.fr       */
+/*   Updated: 2019/09/24 13:31:01 by kheynes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-typedef struct	s_astack
+typedef struct	s_stack
 {
 	int				a;
 	struct s_astack	*next;
-}				t_astack;
-
-typedef struct	s_bstack
-{
-	int				b;
-	struct s_bstack	*next;
-}				t_bstack;
+}				t_stack;
 #endif
